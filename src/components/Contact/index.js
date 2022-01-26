fc66ec5aaeed390e8015ed74e83273c3100c07b7
Sophia Barrett <sophia@yoursummit.media>
2022-01-26 14:34:47 -0700

    update contact form state on user input

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,21 +7,40 @@ function ContactForm() {
     message: "",
   });
 
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
   return (
     <section>
       <h1>Contact Me</h1>
       <form id="contact-form">
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={formState.name} />
+          <input
+            type="text"
+            name="name"
+            defaultValue={formState.name}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={formState.email} />
+          <input
+            type="email"
+            name="email"
+            defaultValue={formState.email}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={formState.message} />
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={formState.message}
+            onChange={handleChange}
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
